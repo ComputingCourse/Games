5db7c9a6e7fa7d809377d42594cada4607d6bc96
ComputingCourse <143533587+ComputingCourse@users.noreply.github.com>
2024-01-18 18:01:43 +0000

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,87 @@
+level_0 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (110,400),
+    "unlock": 1,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/0"}
+level_1 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (300,220),
+    "unlock": 2,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/1"}
+level_2 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (480,610),
+    "unlock": 3,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/2"}
+level_3 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (610,350),
+    "unlock": 4,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/3"}
+level_4 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (880,210),
+    "unlock": 5,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/4"}
+level_5 = {
+    "terrain":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_terrain.csv",
+    "coins":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_coins.csv",
+    "fg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_fg_palms.csv",
+    "bg palms":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_bg_palms.csv",
+    "crates":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_crates.csv",
+    "enemies":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_enemies.csv",
+    "constraints":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_constraints.csv",
+    "player":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_player.csv",
+    "grass":"C:/Users/charl/PycharmProjects/game epesode 2/levels/0/level_0_grass.csv",
+    "node_pos": (1050,400),
+    "unlock": 5,
+    "node_graphics": "C:/Users/charl/PycharmProjects/game epesode 2/graphics/overworld/5"}
+
+levels = {
+    0:level_0,
+    1:level_1,
+    2:level_2,
+    3:level_3,
+    4:level_4,
+    5:level_5
+}
